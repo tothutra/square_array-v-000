e4091060eec70ef06d32ecb9975b044d57849c4d
Tra To <tothutra@gmail.com>
2018-08-12 03:54:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
   new_array = Array.new
   array.each do |element|
     new_element = element ** 2
-    array.unshift
-    array << new_element
+    new_array << new_element
   end
-  return array
+  return new_array
 end
